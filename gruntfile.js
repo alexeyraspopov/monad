2168df066fc605e198a72f6d349b2ac697221142
Avenger7x <avenger7x13@gmail.com>
2014-02-28 18:55:29 +0200

    bump task config

--- gruntfile.js
+++ gruntfile.js
@@ -2,8 +2,18 @@ module.exports = function(grunt){
 	grunt.loadNpmTasks('grunt-contrib-uglify');
 	grunt.loadNpmTasks('grunt-contrib-jshint');
 	grunt.loadNpmTasks('grunt-complexity');
+	grunt.loadNpmTasks('grunt-bump');
 
 	grunt.initConfig({
+		bump: {
+			options: {
+				files: ['package.json', 'bower.json'],
+				commitFiles: ['-a'],
+				commitMessage: 'release %VERSION%',
+				tagMessage: 'version %VERSION%',
+				pushTo: 'origin'
+			}
+		},
 		uglify: {
 			dist: {
 				options: {
